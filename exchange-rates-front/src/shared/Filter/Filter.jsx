b06c3e6cbe7abef93e26293340ace541ead21638
Egor Matsegora <yegor.matsegora@gmail.com>
2021-04-13 17:06:54 +0300

    finish filter

--- exchange-rates-front/src/shared/Filter/Filter.jsx
+++ exchange-rates-front/src/shared/Filter/Filter.jsx
@@ -1,31 +1,50 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import './Filter.sass';
 
 export const Filter = ({filterCurrency, abortFiltration}) => {
   const [queryString, setQueryString] = useState('');
+  const [inputOpenState, setInputOpenState] = useState(false);
+
+  const inputEl = useRef(null);
+
   const handleInputChange = (event) => {
     setQueryString(event.target.value);
     event.target.value ? filterCurrency(queryString) : abortFiltration()
   }
   const handleInputReset = () => {
+  }
+  const handleOpenInput = () => {
+    setInputOpenState(true);
+    inputEl.current.focus();
+  }
+  const handleCloseInput = () => {
     setQueryString('');
     abortFiltration();
+    inputEl.current.blur();
+    setInputOpenState(false);
   }
 
   return (
     <>
       <div className="filter">
-        <input
-          type="text"
-          className="filter__input"
-          value={ queryString }
-          onChange={ handleInputChange }
-        />
+        <div className={`filter__container ${inputOpenState ? 'filter__container--open' : 'filter__container--closed' }`}>
+          <input
+            type="text"
+            className="filter__input"
+            value={ queryString }
+            ref={ inputEl }
+            onChange={ handleInputChange }
+          />
+          <button
+            className="filter__reset filter__button"
+            onClick={ handleCloseInput }
+          ></button>
+        </div>
         <button
-          className="filter__reset"
-          onClick={ handleInputReset }
-        >reset</button>
+          className="filter__open filter__button"
+          onClick={ handleOpenInput }
+        ></button>
       </div>
-      <button className="filter__open">open</button>
     </>
 )
 };
